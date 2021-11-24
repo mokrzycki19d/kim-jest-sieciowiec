2abc640fd485debdc838d0eaf6b6327cf9ac8138
mokrzycki19d <90689734+mokrzycki19d@users.noreply.github.com>
2021-11-24 15:44:57 +0100

    Character Update 2
    -zamieniono grafiki postaci na kreskówkowe
    -utworzono obiekty postaci
    -ulepszono trochę wygląd gry na urządzeniach mobilnych

--- js/main.js
+++ js/main.js
@@ -2,13 +2,36 @@ const qMax = localStorage.getItem('question');
 const button = document.querySelector("#chk");
 const list = document.querySelector("#qlist");
 const ask = document.querySelector("#ask");
+const chars = document.querySelector("#characters");
 
 ask.addEventListener("click", () => {
     if (button.checked) 
     {
         list.style.display = "none";
+        chars.style.display = "grid";
+
     } else 
         {
             list.style.display = "block";
+            if(window.innerWidth <= 1000)
+            {
+                chars.style.display = "none";
+            }
         }
+
 })
+
+class Character
+{
+    constructor(id, sex, hair_colour, hair_length, skin, eyes, glasses, hat)
+    {
+        this.id = id;
+        this.sex = sex;
+        this.hair_colour = hair_colour;
+        this.hair_length = hair_length;
+        this.skin = skin;
+        this.eyes = eyes;
+        this.glasses = glasses;
+        this.hat = hat;
+    }
+}
