9aeece9098bea4df77c9a659e92e0e6e15d27366
mokrzycki19d <90689734+mokrzycki19d@users.noreply.github.com>
2021-11-24 10:28:13 +0100

    Added Question List

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-let qMax = localStorage.getItem('question');
+const qMax = localStorage.getItem('question');
+const button = document.querySelector("#chk");
+const list = document.querySelector("#qlist");
+const ask = document.querySelector("#ask");
+
+ask.addEventListener("click", () => {
+    if (button.checked) 
+    {
+        list.style.display = "none";
+    } else 
+        {
+            list.style.display = "block";
+        }
+})
