0b1ce81d4889f6cd3a701f92edbffc0ae00dc71f
mokrzycki19d <90689734+mokrzycki19d@users.noreply.github.com>
2021-11-29 20:37:30 +0100

    Difficulty Update
    -dodano nowy poziom trudności
    -dodano tekst który pokazuje ile pytań
    ma dany poziom trudności

--- js/diff.js
+++ js/diff.js
@@ -1,6 +1,8 @@
-let button1 = document.querySelector("#easy");
-let button2 = document.querySelector("#medium");
-let button3 = document.querySelector("#hard");
+const button1 = document.querySelector("#easy");
+const button2 = document.querySelector("#medium");
+const button3 = document.querySelector("#hard");
+const button4 = document.querySelector("#cursed");
+const message = document.querySelector("p")
 let question = 0;
 
 button1.addEventListener("click", ()=>{
@@ -8,12 +10,34 @@ button1.addEventListener("click", ()=>{
     localStorage.setItem('question', question);
 })
 
+button1.addEventListener("mouseover", ()=>{
+    message.innerHTML = "Maksymalna liczba pytań: 10";
+})
+
 button2.addEventListener("click", ()=>{
     question = 5;
     localStorage.setItem('question', question);
+})
+
+button2.addEventListener("mouseover", ()=>{
+    message.innerHTML = "Maksymalna liczba pytań: 5";
 })
 
 button3.addEventListener("click", ()=>{
     question = 3;
     localStorage.setItem('question', question);
 })
+
+button3.addEventListener("mouseover", ()=>{
+    message.innerHTML = "Maksymalna liczba pytań: 3";
+})
+
+button4.addEventListener("click", ()=>{
+    question = 1;
+    localStorage.setItem('question', question);
+})
+
+button4.addEventListener("mouseover", ()=>{
+    message.innerHTML = "Maksymalna liczba pytań: 1";
+})
+
